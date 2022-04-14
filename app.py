53d239e609388a704ffd93a77e8158b830e3cd7a
unknown <anjaybisnis01@gmail.com>
2022-04-13 19:26:54 -0700

    Added my project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+# import pickle
+import joblib
+import numpy as np
+model = joblib.load("model/model1.pkl")
+# model = pickle.load(open('model/model_weight_prediction.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data_features = dict(request.form).values()
+    data_features = np.array([float(x) for x in data_features])
+  
+    pred = model.predict([data_features])
+    return render_template('index.html', result=pred)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
